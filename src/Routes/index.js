9fe69e297887b0066698081b3a3fbdeecca8b2aa
rjgohar <rehmatjan143@gmail.com>
2022-08-31 16:27:50 -0700

    add pages

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
+import Home from "../pages/home";
 
 const RoutesMain = () => {
   return (
     <>
       <Router>
         <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
       </Router>
     </>
   );
